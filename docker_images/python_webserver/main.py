06939b3c400efbebbb41928b4977e8ce2c6b8178
Philip Bozakov <w1zard@Philips-MacBook-Pro.local>
2022-10-20 10:16:43 +0300

    pythonweb mysql

--- docker_images/python_webserver/main.py
+++ docker_images/python_webserver/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
-
 from flask import request
-
 import datetime
-
 import sys
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="my-release-mysql.default.svc.cluster.local",
+  user="root",
+  password="uRVld2EFNH",
+  database="pythonweb"
+)
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
@@ -17,9 +23,12 @@ def hello():
 @app.route('/echo')
 
 def echo():
-
+        sql = "INSERT INTO requests (message, timestamp) VALUES (%s, %s)"
+        val = ("test message", format(datetime.datetime.now()))
+        mycursor.execute(sql, val)
+        mydb.commit()
         sys.stderr.write('there was a request at ' + format(datetime.datetime.now()))
-
+        print(mycursor.rowcount, "record inserted.")
         return 'there was a request at ' + format(datetime.datetime.now())
 
 if __name__ == '__main__':
